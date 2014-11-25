5d428fbf9adecd877f6d5b274b6d267d76d63fb6
Ben Schoeggl <benschoeggl@icloud.com>
2014-11-25 11:15:12 -0800

    started express

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,24 @@
 
 'use strict';
 
+var http = require('http');
+
+//var server = http.createServer(function(req, res) {
+//    res.writeHead(200, {'Content-Type': 'textplain'});
+//    res.end('You requested ' + req.url);
+//});
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello world From Ben!');
+});
+
+app.get('/foo', function(req, res) {
+    res.send('You requested /foo!');
+});
+
+app.listen(8080, function() {
+    console.log('Server listening on http://localhost:8080');
+});
